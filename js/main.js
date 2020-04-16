f4d0ff812349ee93640d343f14c7a1ec8c6d9b52
Abel D <proabel.d@gmail.com>
2020-04-17 01:34:49 +0530

    done responsive and sticky menu

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@
     var stickyHeader = document.querySelector('.navbar.app-sticky-top');
     var bottomHeaderSticky = document.querySelector('#navBarMobileSticky');
     var bottomHeader = document.querySelector('#navBarMobile');
+    var HBSWrapper = document.querySelector('.header-bottom-sticky-wrapper');
+    //var toggleHeaderBottomSticky = false;
+    if(window.innerWidth >= 992)
+        toggleHeaderBottomSticky = true;
     document.onscroll = function(e){
-        if(window.scrollY >= 120){
+        if(window.scrollY >= 180){
             stickyHeader.style.display = 'flex';
+            if(toggleHeaderBottomSticky)
+                HBSWrapper.style.display = 'flex';
             bottomHeader.classList.remove('show');
         }else{
             bottomHeaderSticky.classList.remove('show');
             stickyHeader.style.display = 'none';
+            if(toggleHeaderBottomSticky)
+                HBSWrapper.style.display = 'none';
         }
     }
 })();
