3a0901cf3de40839a147d3155c2045cb3a6431fd
Abel D <proabel.d@gmail.com>
2020-04-16 23:09:50 +0530

    working on responsive menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 (function(){
     var stickyHeader = document.querySelector('.navbar.app-sticky-top');
+    var bottomHeaderSticky = document.querySelector('#navBarMobileSticky');
+    var bottomHeader = document.querySelector('#navBarMobile');
     document.onscroll = function(e){
         if(window.scrollY >= 120){
             stickyHeader.style.display = 'flex';
+            bottomHeader.classList.remove('show');
         }else{
+            bottomHeaderSticky.classList.remove('show');
             stickyHeader.style.display = 'none';
         }
     }
